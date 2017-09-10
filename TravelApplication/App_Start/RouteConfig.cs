91400986015392aa8defe26e4bd77a7689613d89
snatarajMetro <natarajS@metro.net>
2017-09-09 23:59:28 -0700

    Added login modal

--- TravelApplication/App_Start/RouteConfig.cs
+++ TravelApplication/App_Start/RouteConfig.cs
@@ -13,10 +13,16 @@ namespace TravelApplication
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "Login",
+            //    defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+            //);
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
