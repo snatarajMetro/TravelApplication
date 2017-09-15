af912ba354fae9a6318339d824fb04989e914ee7
snatarajMetro <natarajS@metro.net>
2017-09-15 14:28:42 -0700

    angularJS addition, work in progress

---
+++ TravelApplication/Scripts/travelrequest.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('travelApp', []);
+
+app.controller('estimatedExpenseCtrl', function ($scope) {
+
+    $scope.parseFloat = function (value) {
+        return parseFloat(value);
+    }
+
+    $scope.advanceLodgingAmount = "0.00";
+    $scope.advanceAirfareAmount = "0.00";
+    //$scope.advanceRegistrationAmount = "0.00";
+    //$scope.advanceMealsAmount = "0.00";
+    //$scope.advanceCarRentalAmount = "0.00";
+    //$scope.advanceMiscellaneousAmount = "0.00";
+    $scope.totalAdvanceAmount = parseFloat($scope.advanceLodgingAmount) + parseFloat($scope.advanceAirfareAmount);// + $scope.advanceRegistrationAmount + $scope.advanceMealsAmount + $scope.advanceCarRentalAmount + $scope.advanceMiscellaneousAmount;
+});
