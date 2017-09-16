7fc6600cf116cb36f7b7d23361f32de8484ce51a
snatarajMetro <natarajS@metro.net>
2017-09-15 19:00:38 -0700

    added angularjs logic

--- TravelApplication/Scripts/travelrequest.js
+++ TravelApplication/Scripts/travelrequest.js
@@ -4,13 +4,24 @@ app.controller('estimatedExpenseCtrl', function ($scope) {
 
     $scope.parseFloat = function (value) {
         return parseFloat(value);
+    };
+
+    $scope.updateTotalAdvanceAmount = function () {
+        $scope.totalAdvanceAmount = (
+            ($scope.advanceLodgingAmount * 1)
+            + ($scope.advanceAirfareAmount * 1)
+            + ($scope.advanceRegistrationAmount * 1)
+            + ($scope.advanceMealsAmount * 1)
+            + ($scope.advanceCarRentalAmount * 1)
+            + ($scope.advanceMiscellaneousAmount * 1)
+            );
     }
 
     $scope.advanceLodgingAmount = "0.00";
     $scope.advanceAirfareAmount = "0.00";
-    //$scope.advanceRegistrationAmount = "0.00";
-    //$scope.advanceMealsAmount = "0.00";
-    //$scope.advanceCarRentalAmount = "0.00";
-    //$scope.advanceMiscellaneousAmount = "0.00";
-    $scope.totalAdvanceAmount = parseFloat($scope.advanceLodgingAmount) + parseFloat($scope.advanceAirfareAmount);// + $scope.advanceRegistrationAmount + $scope.advanceMealsAmount + $scope.advanceCarRentalAmount + $scope.advanceMiscellaneousAmount;
+    $scope.advanceRegistrationAmount = "0.00";
+    $scope.advanceMealsAmount = "0.00";
+    $scope.advanceCarRentalAmount = "0.00";
+    $scope.advanceMiscellaneousAmount = "0.00";
+    
 });
