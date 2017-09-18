6ac29188cc2c7a534ca8b7c384d5e9e45b84f1f1
snatarajMetro <natarajS@metro.net>
2017-09-17 18:36:49 -0700

    Renamed Tests project

---
+++ Tests/TravelRequestServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TravelApplication.Services;
+
+namespace TravelApplication.Tests
+{
+    [TestClass]
+    public class TravelRequestServiceTest
+    {
+        ITravelRequestService travelRequestService = new TravelRequestService();
+
+        [TestMethod]
+        public void GetEmployeeDetailsTest()
+        {
+            int badgeNumber = 93467;
+
+            var response = travelRequestService.GetEmployeeDetails(badgeNumber);
+            Assert.IsNotNull(response);
+        }
+    }
+}
