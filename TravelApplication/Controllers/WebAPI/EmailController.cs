1e9b516825328c9bf09889782ff53e6d2e718ad6
snatarajMetro <natarajS@metro.net>
2017-09-25 16:43:26 -0700

    Added api for Email

---
+++ TravelApplication/Controllers/WebAPI/EmailController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TravelApplication.Services;
+
+namespace TravelApplication.Controllers.WebAPI
+{
+    public class EmailController : ApiController
+    {
+        IEmailService emailService = new EmailService();
+
+        [HttpPost]
+        [Route("api/email/sendemail")]
+        public HttpResponseMessage Email(string fromAddress, string ToAddress, string Subject, string Body)
+        {
+            HttpResponseMessage response = null;
+
+            try
+            {
+
+                var result = emailService.SendEmail(fromAddress, ToAddress, Subject,Body);
+                response = Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log the exception message
+                response = Request.CreateResponse(HttpStatusCode.InternalServerError, "Approve was not sucessfull. Please try again.");
+            }
+
+            return response;
+        }
+    }
+}
---
+++ TravelApplication/Services/IEmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelApplication.Services
+{
+    public interface IEmailService
+    {
+        bool SendEmail(string sendFrom, string sendTo, string subject, string body);
+        bool SendEmail(string sendFrom, string sendTo, string subject, string body,
+                                            string attachmentFile, string cc, string bcc, string smtpServer);
+    }
+}
