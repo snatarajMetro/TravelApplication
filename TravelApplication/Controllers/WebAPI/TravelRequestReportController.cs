1a88053914cd5610f261a56cf704b3a5d892c554
Sharada <natarajs@metro.net>
2017-10-24 15:54:35 -0700

    Added crystal report for view feature - work in progress

---
+++ TravelApplication/Controllers/WebAPI/TravelRequestReportController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TravelApplication.Services;
+
+namespace TravelApplication.Controllers.WebAPI
+{
+    public class TravelRequestReportController : ApiController
+    {
+        TravelRequestReportService travelRequestReportService = new TravelRequestReportService();
+
+        [HttpGet]
+        [Route("api/travelrequestReport/{travelRequestId}")]
+        public HttpResponseMessage GetTravelRequestDetailsNew(string travelRequestId)
+        {
+            HttpResponseMessage response = null;
+            try
+            {
+                Hashtable myparms = new Hashtable();
+                myparms.Add("p_travelrequestID", travelRequestId);
+                travelRequestReportService.RunReport("Travel_Request_newish.rpt","test",myparms);
+               // response = Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log the exception message
+                response = Request.CreateResponse(HttpStatusCode.InternalServerError, "Couldn't retrive travel request details for the given travel request Id : " + ex.Message);
+
+            }
+            return response;
+        }
+    }
+}
---
+++ TravelApplication/Services/TravelRequestReportService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CrystalDecisions.Shared;
+
+namespace TravelApplication.Services
+{
+    public class TravelRequestReportService
+    {
+        public void RunReport(String reportName, String fileName, Hashtable htParameters)
+        {
+            ConnectionInfo crConnectionInFo = new ConnectionInfo();
+            CrystalDecisions.CrystalReports.Engine.Tables crTables;
+            TableLogOnInfo crTableLogonInfo = new TableLogOnInfo();
+            DiskFileDestinationOptions crDiskFileDestinationOptions = new CrystalDecisions.Shared.DiskFileDestinationOptions();
+            ExportOptions crExportOptions = new CrystalDecisions.Shared.ExportOptions();
+
+            CrystalDecisions.CrystalReports.Engine.ReportDocument dReport = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+            String rptPath = System.Web.Hosting.HostingEnvironment.MapPath(System.Configuration.ConfigurationManager.AppSettings["rptPath"]);
+            String fName = fileName;
+            dReport.Load(rptPath + reportName);
+
+            foreach (DictionaryEntry de in htParameters)
+            {
+                String parm_name = System.Convert.ToString(de.Key);  
+                String[] parm_values_array = (System.Convert.ToString(de.Value)).Split(',');
+
+                dReport.SetParameterValue(parm_name, parm_values_array);
+            }
+
+            String mOleString = System.Configuration.ConfigurationManager.AppSettings["rptConnectionString"];
+            String[] aTags = mOleString.Split(';');
+
+            Hashtable hTags = new Hashtable();
+            int tagCount = aTags.Length;
+            for (int cnt = 0; cnt < tagCount - 1; cnt++)
+            {
+                String[] atagParts = aTags[cnt].Split('=');
+                hTags.Add(atagParts[0].Replace("'", ""), atagParts[1].ToUpper().Replace("'", ""));
+            }
+
+            String sPassword = Convert.ToString(hTags["Password"]);
+            String sUserId = Convert.ToString(hTags["User ID"]);
+            String sDataSource = Convert.ToString(hTags["Data Source"]);
+
+            crConnectionInFo.DatabaseName = "";
+            crConnectionInFo.Password = "taer_dev";
+            crConnectionInFo.UserID = "taer";
+            crConnectionInFo.ServerName = "MTAORA50DEV";    
+
+            crTables = dReport.Database.Tables;
+            crTableLogonInfo.ConnectionInfo = crConnectionInFo;
+
+            foreach (CrystalDecisions.CrystalReports.Engine.Table rTable in crTables)
+            {
+                rTable.ApplyLogOnInfo(crTableLogonInfo);
+
+            }
+            foreach (CrystalDecisions.CrystalReports.Engine.ReportDocument subrpt in dReport.Subreports)
+            {
+
+                foreach (CrystalDecisions.CrystalReports.Engine.Table rTable in subrpt.Database.Tables)
+                {
+                    rTable.ApplyLogOnInfo(crTableLogonInfo);
+                }
+            }
+
+            try
+            {
+
+                fName = fName + ".pdf";
+                crDiskFileDestinationOptions.DiskFileName = rptPath + fName;
+                crExportOptions = dReport.ExportOptions;
+
+                crExportOptions.DestinationOptions = crDiskFileDestinationOptions;
+                crExportOptions.ExportDestinationType = ExportDestinationType.DiskFile;
+                crExportOptions.ExportFormatType = ExportFormatType.PortableDocFormat;
+                dReport.Export(crExportOptions);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+
+            }
+            finally
+            {
+                dReport.Close();
+                dReport.Dispose();
+            }
+        }
+    }
+}
