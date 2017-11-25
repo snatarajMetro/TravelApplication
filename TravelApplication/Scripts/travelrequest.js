5fc2a409c47acd2b18b6233df21c60a644aee863
snatarajMetro <natarajS@metro.net>
2017-11-24 19:24:31 -0800

    added pie chart

--- TravelApplication/Scripts/travelrequest.js
+++ TravelApplication/Scripts/travelrequest.js
@@ -2729,6 +2729,8 @@ app.controller('travelAppCtrl', function ($scope, $compile,$timeout) {
             $scope.$apply(function () {
                 loadTravelRequestBarGraph();
                 loadTravelReimbursementBarGraph();
+
+                loadTravelRequestPieChartGraph();
             });
         });
     }
@@ -2942,6 +2944,45 @@ app.controller('travelAppCtrl', function ($scope, $compile,$timeout) {
                 .attr("class", "axisHorizontal")
                 .attr("transform", "translate(" + (margin + labelWidth) + "," + (height - axisMargin - margin) + ")")
                 .call(xAxis);
+    }
+
+    function loadTravelRequestPieChartGraph()
+    {
+        var data = [10, 50, 80, 35];
+        var r = 100;
+
+        var color = d3.scale.ordinal()
+                    .range(["red","blue","orange","green"]);
+
+        var canvas = d3.select('#travelrequestpiechartsection')
+                    .append("svg")
+                    .attr("width", 1500)
+                    .attr("height", 1500);
+
+        var group = canvas.append("g")
+                    .attr("transform", "translate(300,300)");
+
+        var arc = d3.svg.arc()
+        .innerRadius(200)
+        .outerRadius(r);
+
+        var pie = d3.layout.pie()
+        .value(function (d) { return d; });
+
+        var arcs = group.selectAll(".arc")
+                .data(pie(data))
+                .enter()
+                .append("g")
+                .attr("class", "arc");
+
+        arcs.append("path")
+        .attr("d", arc)
+        .attr("fill", function (d) { return color(d.data);});
+
+        arcs.append("text")
+        .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
+        .attr("text-anchor", "middle")
+        .text(function (d) { return d.data; });
     }
 
 });
